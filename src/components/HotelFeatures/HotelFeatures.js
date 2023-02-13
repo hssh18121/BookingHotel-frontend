91fef21ed1b5eefd46a536cb5c4bb76cbd6a22a1
Ha Son <hssh18121@gmail.com>
2023-02-14 00:16:35 +0700

    Refactor featureHelper code

--- src/components/HotelFeatures/HotelFeatures.js
+++ src/components/HotelFeatures/HotelFeatures.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./HotelFeatures.css";
-import { FaSwimmingPool } from "react-icons/fa";
+import { FaParking } from "react-icons/fa";
 import hotelFeaturesHelper from "../../utils/hotelFeaturesHelper";
 const HotelFeatures = (props) => {
   const data = props.hotelDetailData.hotelFeatures;
@@ -21,8 +21,8 @@ const HotelFeatures = (props) => {
           {data.map((el) => {
             return hotelFeaturesHelper(`${el}`, featureData);
           })}
-          {hotelFeaturesHelper("63e6481feaa2b111b0f280b7", featureData)}
-          {hotelFeaturesHelper("63ea37aaaed7d7c92c73391c", featureData)}
+          {hotelFeaturesHelper("unknown", featureData)}
+          {hotelFeaturesHelper("63ea6fe80117e3e38cd5026a", featureData)}
           {/* <div className="reviews__card">
             <div className="reviews__avatar" id="hotel-feature-icon">
               <FaSwimmingPool />
--- src/utils/hotelFeaturesHelper.js
+++ src/utils/hotelFeaturesHelper.js
@@ -1,48 +1,81 @@
 import React, { useState } from "react";
-import { FaAddressBook, FaSwimmingPool, FaWheelchair } from "react-icons/fa";
+import {
+  FaAddressBook,
+  FaCoffee,
+  FaParking,
+  FaQuestion,
+  FaSnowflake,
+  FaSwimmingPool,
+  FaTv,
+  FaUtensils,
+  FaUtensilSpoon,
+  FaWheelchair,
+  FaWifi,
+} from "react-icons/fa";
+const htmlContainer = (component, description) => {
+  return (
+    <React.Fragment>
+      ({" "}
+      <div className="reviews__card hotel-feature-text">
+        <div className="reviews__avatar">Hotel Features</div>
+        <div className="reviews__avatar" id="hotel-feature-icon">
+          {component}
+        </div>
+        <p className="hotel-feature-description">{description}</p>
+      </div>
+      )
+    </React.Fragment>
+  );
+};
 const hotelFeaturesHelper = (id, data) => {
   const feature = data.data?.hotelFeatures.find((el) => el._id === id);
   console.log(feature);
   if (feature?.iconKeyword === "FaSwimmingPool") {
     return (
       <React.Fragment>
-        ({" "}
-        <div className="reviews__card hotel-feature-text">
-          <div className="reviews__avatar">Hotel Features</div>
-          <div className="reviews__avatar" id="hotel-feature-icon">
-            <FaSwimmingPool />
-          </div>
-          <p className="hotel-feature-description">{feature?.description}</p>
-        </div>
-        )
+        {htmlContainer(<FaSwimmingPool />, feature?.description)}
+      </React.Fragment>
+    );
+  } else if (feature?.iconKeyword === "FaSnowflake") {
+    return (
+      <React.Fragment>
+        {htmlContainer(<FaSnowflake />, feature?.description)}
       </React.Fragment>
     );
   } else if (feature?.iconKeyword === "FaWheelchair") {
     return (
       <React.Fragment>
-        ({" "}
-        <div className="reviews__card hotel-feature-text">
-          <div className="reviews__avatar">Hotel Features</div>
-          <div className="reviews__avatar" id="hotel-feature-icon">
-            <FaWheelchair />
-          </div>
-          <p className="hotel-feature-description">{feature?.description}</p>
-        </div>
-        )
+        {htmlContainer(<FaWheelchair />, feature?.description)}
+      </React.Fragment>
+    );
+  } else if (feature?.iconKeyword === "FaWifi") {
+    return (
+      <React.Fragment>
+        {htmlContainer(<FaWifi />, feature?.description)}
+      </React.Fragment>
+    );
+  } else if (feature?.iconKeyword === "FaCoffee") {
+    return (
+      <React.Fragment>
+        {htmlContainer(<FaCoffee />, feature?.description)}
+      </React.Fragment>
+    );
+  } else if (feature?.iconKeyword === "FaTv") {
+    return (
+      <React.Fragment>
+        {htmlContainer(<FaTv />, feature?.description)}
+      </React.Fragment>
+    );
+  } else if (feature?.iconKeyword === "FaParking") {
+    return (
+      <React.Fragment>
+        {htmlContainer(<FaParking />, feature?.description)}
       </React.Fragment>
     );
   } else {
     return (
       <React.Fragment>
-        ({" "}
-        <div className="reviews__card hotel-feature-text">
-          <div className="reviews__avatar">Hotel Features</div>
-          <div className="reviews__avatar" id="hotel-feature-icon">
-            <FaAddressBook />
-          </div>
-          <p className="hotel-feature-description">{feature?.description}</p>
-        </div>
-        )
+        {htmlContainer(<FaUtensils />, "Restaurant Available")}
       </React.Fragment>
     );
   }
